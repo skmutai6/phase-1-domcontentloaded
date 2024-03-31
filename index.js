1d044f769744fd731bdddcaac08c9a5780a20dd1
skmutai6 <skmutai6@gmail.com>
2024-03-31 15:58:18 +0300

    complete lab

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
 // Your code goes here
+
+document.addEventListener("DOMContentLoaded", function() {
+    console.log("The DOM has loaded");
+    const textContent = "JavaScript is so cool. It lets me add text to my page programmatically."
+    const event = document.querySelector( 'text' );
+    text.textContent = "This is really cool!"
+  });
+  
+
